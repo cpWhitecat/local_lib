7cce01bd62f9bb12656f1791d75a76d2581f7ff0
cpWhitecat <19803cp@gmail.com>
2023-05-11 16:04:18 +0800

    show local detail

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,9 +1,33 @@
 const Book = require('../mongodb/models/book.js')
+const Author = require('../mongodb/models/author.js')
+const BookInstance = require('../mongodb/models/bookInstance.js')
+const Genre = require('../mongodb/models/genre.js')
+
+const asyncHandler = require('express-async-handler');
 
 
-exports.index = (req,res)=>{
-    res.send('site')
-}
+exports.index = asyncHandler(async (req,res,next)=>{
+    const [
+        bookCount,
+        bookInstanceCount,
+        authorCount,
+        genreCount
+    ] = await Promise.all([
+        Book.countDocuments().exec(),
+        BookInstance.countDocuments().exec(),
+        Author.countDocuments().exec(),
+        Genre.countDocuments().exec()
+    ])
+
+
+    res.render('index',{
+        title:'this is Apple Chan Local library',
+        book_count:bookCount,
+        bookInstance_count:bookInstanceCount,
+        author_count:authorCount,
+        genre_count:genreCount
+    })
+})
 
 
 // show all book 
