1d3ec081275a131de1db2284cf5ec00e548b6626
cpWhitecat <19803cp@gmail.com>
2023-05-22 20:04:08 +0800

    genre create

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -2,7 +2,8 @@
 const Genre = require('../mongodb/models/genre.js')
 const Book = require('../mongodb/models/book.js')
 const AsyncHandler = require('express-async-handler')
-
+const { body, validationResult } = require('express-validator');
+const { lengthLimited } = require('../utils/index.js');
 
 // show all genre 
 exports.genre_list = AsyncHandler(async (req,res,next)=>{
@@ -42,12 +43,38 @@ exports.genre_detail = AsyncHandler(async (req,res,next)=>{
 
 // create genre
 exports.genre_create_get = AsyncHandler(async (req,res,next)=>{
-    res.send('realistic get function by create')
+    res.render('genre_form',{
+        title:'Create Genre'
+    })
 })
 
-exports.genre_create_post = AsyncHandler(async (req,res,next)=>{
-    res.send('realistic post function by create')
-})
+// want to use ...[] but this is commonjs
+exports.genre_create_post = [  
+    lengthLimited('genre',{min:3}),
+    AsyncHandler(async (req,res,next)=>{
+        const errors = validationResult(req)
+
+        const genre = new Genre({
+            name:req.body.name
+        })
+
+        // because it's errors
+        if(errors.isEmpty()){
+            res.render('genre_form',{
+                title:'Create Genre',
+                genre:genre,
+                errors:errors.array()
+            })
+
+            return
+        }else{
+            // the data is valid
+
+            const hasGenre = await Genre.findOne({name:req.body.name}).exec()
+            res.redirect(genre.url)
+        }
+    })
+]
 
 // delete genre form
 exports.genre_delete_get = AsyncHandler(async (req,res,next)=>{
